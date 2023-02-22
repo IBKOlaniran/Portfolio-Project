e30fe0250507ea32603692b694d0151e09fc6e0d
IBKOlaniran <126057605+IBKOlaniran@users.noreply.github.com>
2023-02-22 17:45:07 +0100

    Add files via upload

--- portfolio project.sql
+++ portfolio project.sql
@@ -1,95 +1,166 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [UniqueID ]
+      ,[ParcelID]
+      ,[LandUse]
+      ,[PropertyAddress]
+      ,[SaleDate]
+      ,[SalePrice]
+      ,[LegalReference]
+      ,[SoldAsVacant]
+      ,[OwnerName]
+      ,[OwnerAddress]
+      ,[Acreage]
+      ,[TaxDistrict]
+      ,[LandValue]
+      ,[BuildingValue]
+      ,[TotalValue]
+      ,[YearBuilt]
+      ,[Bedrooms]
+      ,[FullBath]
+      ,[HalfBath]
+  FROM [PortfolioProject].[dbo].[NashfileHousing]
 
-select location, population, total_deaths
-from PortfolioProject..[covids deaths]
-where continent is not null
-order by 1
+  select SaleDateconverted, CONVERT(date, saledate)
+  from NashfileHousing
 
-
---select *
---from PortfolioProject..[covid vaccination]
-where continent is not null
---order by 3, 4
-
-
-select location, date, total_cases, new_cases, total_deaths, population
-from PortfolioProject..[covids deaths]
-where continent is not null
-order by 1, 2
-
-
-select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
-from PortfolioProject..[covids deaths]
-where continent is not null
-where location like '%Nigeria%'
-order by 1, 2
-
-select location, date, population, total_cases, (total_cases/population)*100 as PercentPopulationInfected
-from PortfolioProject..[covids deaths]
---where location like '%Nigeria%'
-order by 1, 2
+  select SaleDate, CONVERT(date, saledate)
+  from NashfileHousing
 
 
-select location, population, MAX(total_cases) as HighestInfectionCount, Max((total_cases/population))*100 as PercentPopuationInfected
-from PortfolioProject..[covids deaths]
---where location like '%Nigeria%'
-group by location, population
-order by PercentPopuationInfected desc
+  alter table nashfilehousing
+  add Saledateconverted date;
+
+  update NashfileHousing
+  set SaleDateconverted = CONVERT(date, saledate)
 
 
-select location, MAX(cast(total_deaths as int)) as TotalDeathCount 
-from PortfolioProject..[covids deaths]
---where location like '%Nigeria%'
-where continent is not null
-group by location
+  --property address
+  select*
+  from NashfileHousing
+  where PropertyAddress is null
+  order by ParcelID
 
 
-select continent, MAX(cast(total_deaths as int)) as TotalDeathCount 
-from PortfolioProject..[covids deaths]
---where location like '%Nigeria%'
-where continent is not null
-group by continent
-order by TotalDeathCount desc
+  select*
+  from NashfileHousing
+  where PropertyAddress is not null
+  order by ParcelID
+
+select a.parcelid, a.propertyaddress, b.parcelid, b.propertyaddress,b.propertyaddress, ISNULL(a.propertyaddress, b.propertyaddress)
+from NashfileHousing a
+join NashfileHousing b
+on a.parcelid = b.parcelid
+and a.[uniqueid] <> b.[uniqueid]
+where a.propertyaddress is null
+
+update a
+set propertyaddress = ISNULL(a.propertyaddress, b.propertyaddress)
+from NashfileHousing a
+join NashfileHousing b
+on a.parcelid = b.parcelid
+and a.[uniqueid] <> b.[uniqueid]
+where a.propertyaddress is null
+
+
+select propertyaddress
+from NashfileHousing
+
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as Address
+
+from NashfileHousing
+
+ALTER TABLE nashfilehousing
+Add PropertySplitAddress Nvarchar(255);
+
+Update NashfileHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
+
+
+ALTER TABLE nashfilehousing
+Add PropertySplitCity Nvarchar(255);
+
+Update NashfileHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress))
+ 
+ 
+Select *
+From NashfileHousing
+
+
+select owneraddress
+from NashfileHousing
+where OwnerAddress is not null 
+
+
+select owneraddress
+from NashfileHousing
+
+Select
+PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+From NashfileHousing
 
 
 
-select SUM (new_cases)as total_cases, sum (cast(new_deaths as int)) as total_deaths, sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPercentage
-from PortfolioProject..[covids deaths]
-where continent is not null
---where location like '%Nigeria%'
---group by date
-order by 1, 2
+Update NashfileHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+
+ALTER TABLE NashfileHousing
+Add OwnerSplitCity Nvarchar(255);
+
+Update NashfileHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+
+
+ALTER TABLE NashfileHousing
+Add OwnerSplitState Nvarchar(255);
+
+Update NashfileHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
 
 
 
-select date, SUM (new_cases)as total_cases, sum (cast(new_deaths as int)) as total_deaths, sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPercentage
-from PortfolioProject..[covids deaths]
-where continent is not null
---where location like '%Nigeria%'
-group by date
-order by 1, 2
+Select *
+From NashfileHousing
 
-with popvsvac (continent, location, date, population, total_vaccination, RollingPeopleVaccinated)
-as
-(
-select dea.continent, dea.location, dea.date, dea.population, vac.total_vaccinations
-, sum(convert(int,vac.total_vaccinations)) over (partition by dea.location order by dea.location, dea.date)
-from PortfolioProject..[covids deaths] dea
-join PortfolioProject..[covid vaccination] vac
-	on dea.location = vac.location
-	and dea.date = vac.date
-where dea.continent is not null
-)
+
+select distinct (soldasvacant)
+from NashfileHousing
+
+
+
+select distinct (soldasvacant), count (soldasvacant)
+from NashfileHousing
+
+group by SoldAsVacant
+order by 2
+
+
+select soldasvacant
+, case when soldasvacant = 'Y' then 'Yes'
+when soldasvacant = 'N' then 'No'
+else soldasvacant
+end
+from nashfilehousing
+
+
+Update nashfilehousing
+SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+
 select *
-from popvsvac
+from NashfileHousing
 
-create view percentagepopulationvaccinated as
-select dea.continent, dea.location, dea.date, dea.population, vac.total_vaccinations
-, sum(convert(int,vac.total_vaccinations)) over (partition by dea.location order by dea.location, 
-dea.date) as rollingpeoplevaccinated
-from PortfolioProject..[covids deaths] dea
-join PortfolioProject..[covid vaccination] vac
-	on dea.location = vac.location
-	and dea.date = vac.date
-where dea.continent is not null
 
+alter table nashfilehousing
+drop column taxdistrict
+
+
+select *
+from NashfileHousing
 
